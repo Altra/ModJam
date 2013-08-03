d8ff91c31a8bb49f91b90de02f798086061c4d50
Altra <flyhiy1@gmail.com>
2013-08-03 19:13:13 +0100

    Back later

--- Altra/ModJam/client/model/ModelDwarf.java
+++ Altra/ModJam/client/model/ModelDwarf.java
@@ -22,85 +22,85 @@ public class ModelDwarf extends ModelBase
     ModelRenderer beard6;
     ModelRenderer beard7;
   
-  public ModelDwarf(float f){
-	  int inc = (int) f;
+  public ModelDwarf(float f)
+  {
     textureWidth = 64;
     textureHeight = 32;
     
       beard1 = new ModelRenderer(this, 0, 0);
-      beard1.addBox(-3F, 0F, 0F, 5, 2, 1);
+      beard1.addBox(-3F, 0F, 3F, 5, 2, 1);
       beard1.setRotationPoint(0F, 8F, -9F);
       beard1.setTextureSize(64, 32);
       beard1.mirror = true;
       setRotation(beard1, 0F, 0F, 0F);
       head = new ModelRenderer(this, 34, 22);
-      head.addBox(-4F, -4F, -3F, 5, 5, 5);
+      head.addBox(-4F, -4F, 0F, 5, 5, 5);
       head.setRotationPoint(1F, 9F, -5F);
       head.setTextureSize(64, 32);
       head.mirror = true;
       setRotation(head, 0F, 0F, 0F);
       body = new ModelRenderer(this, 14, 7);
-      body.addBox(-5F, 0F, -6F, 9, 6 + inc , 9);
+      body.addBox(-5F, 0F, -3F, 9, 6, 9);
       body.setRotationPoint(0F, 13F, -9F);
       body.setTextureSize(64, 32);
       body.mirror = true;
       setRotation(body, 1.570796F, 0F, 0F);
       leg1 = new ModelRenderer(this, 0, 23);
-      leg1.addBox(-2F, 0F, -3F, 3, 5 + inc, 4);
+      leg1.addBox(-2F, 0F, 0F, 3, 5, 4);
       leg1.setRotationPoint(-3F, 19F, -5F);
       leg1.setTextureSize(64, 32);
       leg1.mirror = true;
       setRotation(leg1, 0F, 0F, 0F);
       leg2 = new ModelRenderer(this, 0, 14);
-      leg2.addBox(-2F, 0F, -3F, 3, 5 + inc, 4);
+      leg2.addBox(-2F, 0F, 0F, 3, 5, 4);
       leg2.setRotationPoint(3F, 19F, -5F);
       leg2.setTextureSize(64, 32);
       leg2.mirror = true;
       setRotation(leg2, 0F, 0F, 0F);
       arm2 = new ModelRenderer(this, 24, 22);
-      arm2.addBox(0F, 0F, -2F, 2, 7, 3);
+      arm2.addBox(0F, 0F, +1F, 2, 7, 3);
       arm2.setRotationPoint(4F, 11F, -5F);
       arm2.setTextureSize(64, 32);
       arm2.mirror = true;
       setRotation(arm2, 0F, 0F, 0F);
       arm1 = new ModelRenderer(this, 14, 22);
-      arm1.addBox(-2F, 0F, -1F, 2, 7, 3);
+      arm1.addBox(-2F, 0F, 2F, 2, 7, 3);
       arm1.setRotationPoint(-5F, 11F, -6F);
       arm1.setTextureSize(64, 32);
       arm1.mirror = true;
       setRotation(arm1, 0F, 0F, 0F);
       beard2 = new ModelRenderer(this, 20, 1);
-      beard2.addBox(-3F, 0F, 0F, 1, 2, 4);
+      beard2.addBox(-3F, 0F, 3F, 1, 2, 4);
       beard2.setRotationPoint(5F, 8F, -8F);
       beard2.setTextureSize(64, 32);
       beard2.mirror = true;
       setRotation(beard2, 0F, 0F, 0F);
       beard3 = new ModelRenderer(this, 11, 0);
-      beard3.addBox(0F, 0F, -1F, 1, 2, 4);
+      beard3.addBox(0F, 0F, 2F, 1, 2, 4);
       beard3.setRotationPoint(-4F, 8F, -7F);
       beard3.setTextureSize(64, 32);
       beard3.mirror = true;
       setRotation(beard3, 0F, 0F, 0F);
       beard5 = new ModelRenderer(this, 0, 0);
-      beard5.addBox(-2F, -1F, -1F, 3, 1, 1);
+      beard5.addBox(-2F, -1F, 2F, 3, 1, 1);
       beard5.setRotationPoint(0F, 15F, -9F);
       beard5.setTextureSize(64, 32);
       beard5.mirror = true;
       setRotation(beard5, 0F, 0F, 0F);
       beard4 = new ModelRenderer(this, 0, 7);
-      beard4.addBox(-3F, -1F, -1F, 5, 5, 2);
+      beard4.addBox(-3F, -1F, 2F, 5, 5, 2);
       beard4.setRotationPoint(0F, 10F, -9.2F);
       beard4.setTextureSize(64, 32);
       beard4.mirror = true;
       setRotation(beard4, 0F, 0F, 0F);
       beard6 = new ModelRenderer(this, 0, 0);
-      beard6.addBox(-1F, 0F, -1F, 1, 3, 1);
+      beard6.addBox(-1F, 0F, 2F, 1, 3, 1);
       beard6.setRotationPoint(-3F, 10F, -9F);
       beard6.setTextureSize(64, 32);
       beard6.mirror = true;
       setRotation(beard6, 0F, 0F, 0F);
       beard7 = new ModelRenderer(this, 0, 0);
-      beard7.addBox(-1F, 0F, -1F, 1, 3, 1);
+      beard7.addBox(-1F, 0F, 2F, 1, 3, 1);
       beard7.setRotationPoint(3F, 10F, -9F);
       beard7.setTextureSize(64, 32);
       beard7.mirror = true;
@@ -143,6 +143,8 @@ public class ModelDwarf extends ModelBase
 	  this.arm2.rotateAngleX = MathHelper.cos(par1 * 0.6662F) * 1.7F * par2 * 0.5F;
       this.leg1.rotateAngleX = MathHelper.cos(par1 * 0.6662F) * 1.7F * par2 * 0.5F;
       this.leg2.rotateAngleX = MathHelper.cos(par1 * 0.6662F + (float)Math.PI) * 1.7F * par2 * 0.5F;
+      
+      this.head.offsetY = MathHelper.cos(par1 * 0.6662F)/2;
   }
 
 }
