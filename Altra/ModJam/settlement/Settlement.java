b83fa1426fa239cbec9876349c07256a3b86f1f7
Altra <flyhiy1@gmail.com>
2013-08-03 12:57:28 +0100

    More Settlement work

--- Altra/ModJam/settlement/Settlement.java
+++ Altra/ModJam/settlement/Settlement.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeMap;
 
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.ChunkCoordinates;
 import net.minecraft.world.World;
+import Altra.ModJam.entity.EntityDwarf;
 
 public class Settlement {
 	private World worldObj;
@@ -15,6 +17,8 @@ public class Settlement {
 	/** Settlement Centre */
 	private final ChunkCoordinates center = new ChunkCoordinates(0, 0, 0);
 	private int settlementRadius;
+	private String settlementType;
+	private String populationEntity;
 	private int lastAddDoorTimestamp;
 	private int tickCounter;
     private int population;
@@ -25,8 +29,33 @@ public class Settlement {
     private TreeMap playerReputation = new TreeMap();
     private List villageAgressors = new ArrayList();
     
-    
-    
+    public Settlement(World world){
+    	this.worldObj = world;
+    }
+
+    public void tick(int tick){
+    	this.tickCounter = tick;
+    	//this.removeDeadAndOutOfRangeDoors();
+    	//this.removeDeadAndOldAgressors();
+
+    	if (tick % 20 == 0){
+    		this.updatePopulation();
+    	}
+
+    }
+
+    private void updatePopulation(){
+    	List list = null;
+    	if(this.settlementType=="dwarven"){
+    		list = this.worldObj.getEntitiesWithinAABB(EntityDwarf.class, AxisAlignedBB.getAABBPool().getAABB((double)(this.center.posX - this.settlementRadius), (double)(this.center.posY - 30), (double)(this.center.posZ - this.settlementRadius), (double)(this.center.posX + this.settlementRadius), (double)(this.center.posY + 30), (double)(this.center.posZ + this.settlementRadius)));
+    	}
+    	
+    	if(list!=null)this.population = list.size();
+
+    	if (this.population == 0){
+    		this.playerReputation.clear();
+    	}
+    }
 
 
 }
