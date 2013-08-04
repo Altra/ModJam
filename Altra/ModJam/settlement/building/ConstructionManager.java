9c6a8d4a36b90ccd7daf090d41d5aaebe04b5903
Altra <flyhiy1@gmail.com>
2013-08-04 15:53:03 +0100

    More construction stuff

--- Altra/ModJam/settlement/building/ConstructionManager.java
+++ Altra/ModJam/settlement/building/ConstructionManager.java
@@ -3,53 +3,69 @@ package Altra.ModJam.settlement.building;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.ai.RandomPositionGenerator;
 import net.minecraft.util.Vec3;
+import net.minecraft.world.World;
 
 public class ConstructionManager {
+	
+	private Random rand = new Random();
 
 	private  Building building;
-	private final List workerEntitys = new ArrayList();
+	private final List workers = new ArrayList();
 	
 	private int tickCounter = 0;
+	public boolean completed;
+	
+	public int centerX;
+	public int centerY;
+	public int centerZ;
+	
+	public int globalCurrentX;
+	public int globalCurrentY;
+	public int globalCurrentZ;
 
-	public ConstructionManager(Building B){
+	public ConstructionManager(Building B, int x, int y, int z){
 		this.building = B;
+		this.centerX = x;
+		this.centerY = y;
+		this.centerZ = z;
 	}
 
 	public void addWorkerEntity(EntityCreature entity){
-		this.workerEntitys.add(entity);
+		this.workers.add(new Worker(entity));
+	}
+	
+	private void coordUpdate(){
+		this.globalCurrentX = this.centerX + rand.nextInt(building.lengthX) - rand.nextInt(building.lengthX);
+		this.globalCurrentY = this.centerY + rand.nextInt(building.noLevels) - rand.nextInt(building.noLevels);
+		this.globalCurrentZ = this.centerZ + rand.nextInt(building.lengthZ) - rand.nextInt(building.lengthZ);
 	}
 
 	public void tick(){
 		++this.tickCounter;
-		Iterator iterator = this.workerEntitys.iterator();
+		Iterator iterator = this.workers.iterator();
 
 		while (iterator.hasNext()){
-			EntityCreature worker = (EntityCreature)iterator.next();
-			worker
+			coordUpdate();
+			Worker worker = (Worker)iterator.next();
+			worker.setNavigator(this.globalCurrentX, this.globalCurrentY, this.globalCurrentZ);
 		}
 
-		//this.removeAnnihilatedSettlements();
-		this.addNewDoorsToSettlement();
-
-		if(this.tickCounter % 400 == 0){
-			this.markDirty();
-		}
 	}
-
-	private void setNavigator(EntityCreature worker, int x, int y, int z){
-		if (worker.getDistanceSq(x, y, z) > 256.0D){
-			Vec3 vec3 = RandomPositionGenerator.findRandomTargetBlockTowards(worker, 14, 3, worker.worldObj.getWorldVec3Pool().getVecFromPool(x, y, z));
-
-			if (vec3 != null){
-				worker.getNavigator().tryMoveToXYZ(vec3.xCoord, vec3.yCoord, vec3.zCoord, 1.0D);
+	
+	private boolean isWorkerNearBy(int x, int y, int z){
+		Iterator iterator = this.workers.iterator();
+		while (iterator.hasNext()){
+			Worker worker = (Worker)iterator.next();
+			if(worker.getDistanceSquared(x, y, z)<10){
+				return true;
 			}
-		} else{
-			worker.getNavigator().tryMoveToXYZ(x, y, z, 1.0D);
 		}
+		return false;
 	}
 
 }
---
+++ Altra/ModJam/settlement/building/Worker.java
@@ -0,0 +1,38 @@
+package Altra.ModJam.settlement.building;
+
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.ai.RandomPositionGenerator;
+import net.minecraft.util.Vec3;
+
+public class Worker{
+
+	EntityCreature entity;
+
+	public int targetX;
+	public int targetY;
+	public int targetZ;
+
+	Worker(EntityCreature e){
+		this.entity = e;
+	}
+
+	public void setNavigator(int x, int y, int z){
+		if (this.entity.getDistanceSq(x, y, z) > 256.0D){
+			Vec3 vec3 = RandomPositionGenerator.findRandomTargetBlockTowards(this.entity, 14, 3, this.entity.worldObj.getWorldVec3Pool().getVecFromPool(x, y, z));
+
+			if (vec3 != null){
+				this.entity.getNavigator().tryMoveToXYZ(vec3.xCoord, vec3.yCoord, vec3.zCoord, 1.0D);
+			}
+		} else{
+			this.entity.getNavigator().tryMoveToXYZ(x, y, z, 1.0D);
+		}
+	}
+	
+   public int getDistanceSquared(int x, int y, int z){
+       int X = (int) (x - this.entity.posX);
+       int Y = (int) (y - this.entity.posY);
+       int Z = (int) (z - this.entity.posZ);
+       return X* X + Y * Y + Z * Z;
+   }
+
+}
